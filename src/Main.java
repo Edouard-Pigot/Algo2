22c5d5167d4ab872b37426767d30657375bea7ab
Raphael Charpy <raphael.charpy@etu.univ-amu.fr>
2018-11-02 15:10:45 +0100

    Lecture du dico.txt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by RAPHAEL on 02/11/2018.
  */
 public class Main {
     public static void main(String[] args){
+        try{
+            Scanner sc = new Scanner(new File("src/dico.txt"));
+            while (sc.hasNext()) {
+                System.out.println(sc.next());
+            }
+        } catch (FileNotFoundException e){
+            System.out.println(e);
+        }
+
+
         System.out.println("Ca marche ?");
+
     }
 }
