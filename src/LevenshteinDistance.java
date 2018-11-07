f402d0658468fa7a711507372ed0ffb839abebea
c15009878 <raphael.charpy@etu.univ-amu.fr>
2018-11-07 09:57:18 +0100

    Début de levenshtein

--- src/LevenshteinDistance.java
+++ src/LevenshteinDistance.java
@@ -2,4 +2,49 @@
  * Created by RAPHAEL on 02/11/2018.
  */
 public class LevenshteinDistance {
+
+    public LevenshteinDistance() {
+    }
+
+    public int calculation(char[] falseString, char[] trueString) {
+        int[] d ;
+        int costSub;
+        for (int i= 0 ; i < falseString.length;++i){
+            for(int j=0; j < trueString.length;j++){
+
+            }
+        }
+        return 1;
+
+    }
+//
+//    public String insert(String falseString, String trueString){
+//        for(int i = 0; i < falseString.length();++i) {
+//            for (int j = 0; j < trueString.length(); ++j) {
+//                if(falseString.charAt(i) != trueString.charAt(j)){
+//                    falseString = trueString.charAt(j) + falseString;
+//                    i = 0;
+//                    j=0;
+//                    return  falseString;
+//                }
+//            }
+//        }
+//        return  falseString;
+//
+//    }
+//
+//    public String suppr(String falseString, String trueString){
+//        for(int i = 0; i < falseString.length();++i) {
+//            for (int j = 0; j < trueString.length(); ++j) {
+//                if(falseString.charAt(i) != trueString.charAt(j)){
+//                    falseString ;
+//                    i = 0;
+//                    j=0;
+//                    return  falseString;
+//                }
+//            }
+//        }
+//        return falseString;
+//    }
+
 }
