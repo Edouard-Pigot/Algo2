d8827ed149557f5edc9a99051c3395eb8fff5e4c
Raphael Charpy <raphael.charpy@etu.univ-amu.fr>
2018-11-13 19:14:15 +0100

    Levenshtein et main

--- src/LevenshteinDistance.java
+++ src/LevenshteinDistance.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * Created by RAPHAEL on 02/11/2018.
  */
@@ -6,15 +8,33 @@ public class LevenshteinDistance {
     public LevenshteinDistance() {
     }
 
-    public int calculation(char[] falseString, char[] trueString) {
-        int[] d ;
+    public int calculation(String falseString, String trueString) {
+        int distance[][] = new int[0][0];
         int costSub;
-        for (int i= 0 ; i < falseString.length;++i){
-            for(int j=0; j < trueString.length;j++){
+        for (int i= 0 ; i < falseString.length();++i){
+            distance[i][0] = i;
+        }
+        for(int j=0; j < trueString.length();++j){
+            distance[0][j] = j;
+        }
+
+        for (int i = 1 ; i < falseString.length();++i){
+            for(int j= 1 ; j<trueString.length();++j){
+                if(falseString.charAt(i-1) == trueString.charAt(j-1)){
+                    costSub = 0;
+                } else {
+                    costSub = 1;
+                }
+
+                int min1= Math.min(distance[i-1][j]+1,distance[i][j-1]+1);
+                int min2 = Math.min(min1,distance[i-1][j-1]+costSub);
+                distance[i][j] = min2;
+
+
 
             }
         }
-        return 1;
+        return distance[falseString.length()][trueString.length()];
 
     }
 //
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -8,10 +10,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         try{
+            List<String> dictionnaire = new ArrayList<>();
             Scanner sc = new Scanner(new File("src/dico.txt"));
             while (sc.hasNext()) {
-                System.out.println(sc.next());
+                dictionnaire.add(sc.next());
+
+                //System.out.println(sc.next());
             }
+            System.out.println(dictionnaire);
         } catch (FileNotFoundException e){
             System.out.println(e);
         }
